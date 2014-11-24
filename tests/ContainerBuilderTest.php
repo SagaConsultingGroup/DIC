2673072c70cd92663c117706bc7a1358057a381e
Dries De Peuter <dries@nousefreak.be>
2014-11-24 23:24:28 +0100

    Add ContainerCache tests.

--- tests/ContainerBuilderTest.php
+++ tests/ContainerBuilderTest.php
@@ -10,12 +10,39 @@ use Drupal\dic\DICBuilder;
 
 class ContainerBuilderTest extends \PHPUnit_Framework_TestCase {
 
+  private function clearContainerCache() {
+    if (is_file($this->getContainerPath())) {
+      unlink($this->getContainerPath());
+    }
+  }
+
+  private function getContainerPath() {
+    return __DIR__ . '/Resources/.container/container.php';
+  }
+
   /**
    * @covers Drupal\dic\DICBuilder::build
    */
   public function testContainerBuild() {
-    $builder = new DICBuilder(array(), './tests/Resources/.container/container.php');
+    $this->clearContainerCache();
+
+    $builder = new DICBuilder(array(), $this->getContainerPath());
     $container = $builder->build();
     $this->assertInstanceOf('Symfony\Component\DependencyInjection\ContainerInterface', $container);
+  }
+
+  /**
+   * @covers Drupal\dic\DICBuilder::cacheContainer
+   */
+  public function testContainerBuildCache() {
+    $this->clearContainerCache();
+
+    $builder = new DICBuilder(array(), $this->getContainerPath());
+    $container = $builder->build();
+
+    $this->assertFileExists($this->getContainerPath());
+
+    $reflection = new \ReflectionClass($container);
+    $this->assertEquals($this->getContainerPath(), $reflection->getFileName());
   }
 }
